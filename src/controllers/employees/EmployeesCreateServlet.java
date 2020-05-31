bb8a9ff57c6cfc5137e17bcd27c2df01e52c3f9d
kazuki.matsuura <ptpkazu0510@yahoo.co.jp>
2020-05-31 21:31:10 +0900

    Fix task 1

--- src/controllers/employees/EmployeesCreateServlet.java
+++ src/controllers/employees/EmployeesCreateServlet.java
@@ -62,7 +62,7 @@ public class EmployeesCreateServlet extends HttpServlet {
                 em.close();
 
                 request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("errors", errors);
+                request.setAttribute("employee", e);
                 request.setAttribute("errors", errors);
 
                 RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
